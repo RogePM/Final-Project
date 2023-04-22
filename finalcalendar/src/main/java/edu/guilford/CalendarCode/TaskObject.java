9fbca515661144fe74e05b8979d6a1f728195188
Rogelio Perez-Montero <131227318+RogePM@users.noreply.github.com>
2023-04-21 23:29:45 -0400

    Add files via upload

---
+++ finalcalendar/src/main/java/edu/guilford/CalendarCode/TaskObject.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.guilford.CalendarCode;
+
+/**
+ *
+ * @author rogel
+ */
+public class TaskObject {
+    String event;
+    String note;
+    String date;
+
+    public TaskObject(String event, String note, String date) {
+        this.event = event;
+        this.note = note;
+        this.date = date;
+    }
+    
+    
+}
